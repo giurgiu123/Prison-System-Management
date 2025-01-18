54bf19e9004ccf717197ba278bd8445ec1be5dae
giurgiupaul <80486033+giurgiu123@users.noreply.github.com>
2025-01-18 22:10:44 +0200

    Delete procedures directory

--- procedures/procedure_years_between_dates.sql
+++
@@ -1,4 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `years_between_dates`(IN sta DATETIME, IN fin DATETIME, OUT yea INTEGER)
-BEGIN
-	SELECT TIMESTAMPDIFF(year , sta, fin) into yea;
-END
--- procedures/query_all_employees_that_have_accounts_and_shift.sql
+++
@@ -1,8 +0,0 @@
-# sa vad shift-ul fiecarui gardian
-# select: name, everything about the shift
-select utilizator.username, gardian.fk_id_utilizator,  shift.id_shift, shift.fk_id_etaj, shift.tip_shift from gardian join shift on shift.id_shift = gardian.fk_id_shift 
-										join utilizator on gardian.fk_id_utilizator = utilizator.id_utilizator
-
-
-# toti detinutii de la un anumit gardian
-							
